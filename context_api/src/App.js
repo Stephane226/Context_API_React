2caa528f5b41fd1e2b41aa6d08505b61c14fea2e
Masaaki Stephane <bstephane97@yandex.com>
2022-03-07 01:37:38 +0300

    Context.Provider

--- context_api/src/App.js
+++ context_api/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js';  
 import BookList from './components/booklist';
+import ThemeContextProvider from './context/themeContext';
+
 
 function App() {
   return (
     <div className="App">
- <Navbar/>
- <BookList/>
+      <ThemeContextProvider>
+          
+            <Navbar/>
+            <BookList/>
+
+      </ThemeContextProvider>     
     </div>
   );
 }
